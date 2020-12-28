23237a3352100452bc227ee7f607a4a38964c9df
haoyuwu36 <haoyuwu36@gmail.com>
2020-12-27 22:17:18 -0500

    add touching annotations for rw201-210

--- room world/testLabel.py
+++ room world/testLabel.py
@@ -10,11 +10,12 @@ def train(epochs):
     for epoch in range(epochs):
         scene = scenes[scene_idx]
         name = scene.split(".blend")[0] + '.data'
-        if name in annotations:
-            #command = ['/Applications/Blender.app/Contents/MacOS/Blender', "scenes" + os.sep + scene, '--background', '-P',
-            #           'testLabel2.py', '--',"annotations" + os.sep + name]
-            command = ['C:\\Program Files\\Blender Foundation\\Blender 2.90\\blender.exe', "scenes" + os.sep + scene, '--background', '-P',
-                       'testLabel2.py', '--', "annotations" + os.sep + name]
+        if name in annotations and (name in ['RW201.data','RW202.data','RW203.data','RW204.data','RW205.data','RW206.data',
+            'RW207.data','RW208.data','RW209.data','RW210.data']):
+            command = ['/Applications/Blender.app/Contents/MacOS/Blender', "scenes" + os.sep + scene, '--background', '-P',
+                       'testLabel2.py', '--',"annotations" + os.sep + name]
+            #command = ['C:\\Program Files\\Blender Foundation\\Blender 2.90\\blender.exe', "scenes" + os.sep + scene, '--background', '-P',
+            #           'testLabel2.py', '--', "annotations" + os.sep + name]
             #command = ['../blender/blender', "scenes" + os.sep + scene, '--background', '-P',
             #           'testLabel2.py', '--', "annotations" + os.sep + name]
             subprocess.run(command)
